f160cacb7bcfa6963e4cf073e715b31d950c4f7c
Scarecr0w9758 <132538423+Scarecr0w9758@users.noreply.github.com>
2023-05-16 10:03:32 +0500

    feat: aadded vue-widgets and add template to element

---
+++ PriceWidgetElement.vue
@@ -0,0 +1,25 @@
+<template>
+    <div role="content-holder">
+        <div role="image-holder">
+            <img />
+        </div>
+        <div role="title">
+            <h3>{{props.title}}</h3>
+        </div>
+        <div role="text">
+            <p>{{props.subTitle}}</p>
+        </div>
+        <div role="price">
+            <p>{{props.price}}</p>
+        </div>
+    </div>
+    <div role="button chose">
+        <button class="subscribeButton"> 
+            то, что нужно
+        </button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
